7efa5d688b516966eaaf1ee528b66af4cb32ea54
Minjae <cyranokim@gmail.com>
2023-09-25 19:49:17 +0900

    Add DummySkills

---
+++ Assets/Scripts/Managers/Game/DummySkills.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DummySkills
+{
+    
+}
+
+public class DummyFireballSkill : SkillBase
+{
+    public DummyFireballSkill()
+    {
+        Id                = 1;
+        Type              = SkillType.Attack;
+        Priority          = 1;
+        IsRestricteMoving = false;
+        Cooldown          = 5f;
+        BeforeDelay       = 0.5f;
+        Duration          = 2f;
+        AfterDelay        = 0.5f;
+    }
+}
+
+public class DummyHealSkill : SkillBase
+{
+    
+    public DummyHealSkill()
+    {
+        Id                = 2;
+        Type              = SkillType.Defence;
+        Priority          = 2;
+        IsRestricteMoving = false;
+        Cooldown          = 10f;
+        BeforeDelay       = 0f;
+        Duration          = 1f;
+        AfterDelay        = 0f;
+    }
+}
+
+public class DummyStunSkill : SkillBase
+{
+    public DummyStunSkill()
+    {
+        Id                = 3;
+        Type              = SkillType.Attack;
+        Priority          = 3;
+        IsRestricteMoving = true;
+        Cooldown          = 8f;
+        BeforeDelay       = 0.2f;
+        Duration          = 1.5f;
+        AfterDelay        = 0.2f;
+    }
+}
+
+public class DummyShieldSkill : SkillBase
+{
+    public DummyShieldSkill()
+    {
+        Id                = 4;
+        Type              = SkillType.Defence;
+        Priority          = 1;
+        IsRestricteMoving = false;
+        Cooldown          = 15f;
+        BeforeDelay       = 0f;
+        Duration          = 4f;
+        AfterDelay        = 0f;
+    }
+}
+
+public class DummyDashSkill : SkillBase
+{
+    public DummyDashSkill()
+    {
+        Id                = 5;
+        Type              = SkillType.General;
+        Priority          = 2;
+        IsRestricteMoving = false;
+        Cooldown          = 3f;
+        BeforeDelay       = 0f;
+        Duration          = 0.5f;
+        AfterDelay        = 0f;
+    }
+}
+
+public class DummyLightningSkill : SkillBase
+{
+    public DummyLightningSkill()
+    {
+        Id                = 6;
+        Type              = SkillType.Attack;
+        Priority          = 3;
+        IsRestricteMoving = false;
+        Cooldown          = 4f;
+        BeforeDelay       = 0.1f;
+        Duration          = 1.2f;
+        AfterDelay        = 0.3f;
+    }
+}
+
+public class DummyIceSkill : SkillBase
+{
+    public DummyIceSkill()
+    {
+        Id                = 7;
+        Type              = SkillType.Attack;
+        Priority          = 1;
+        IsRestricteMoving = true;
+        Cooldown          = 6f;
+        BeforeDelay       = 0.4f;
+        Duration          = 2.5f;
+        AfterDelay        = 0.4f;
+    }
+}
+
+public class DummyStealthSkill : SkillBase
+{
+    public DummyStealthSkill()
+    {
+        Id                = 8;
+        Type              = SkillType.General;
+        Priority          = 2;
+        IsRestricteMoving = false;
+        Cooldown          = 10f;
+        BeforeDelay       = 0f;
+        Duration          = 5f;
+        AfterDelay        = 0f;
+    }
+}
+
+public class DummyBarrierSkill : SkillBase
+{
+    public DummyBarrierSkill()
+    {
+        Id                = 9;
+        Type              = SkillType.Defence;
+        Priority          = 3;
+        IsRestricteMoving = false;
+        Cooldown          = 12f;
+        BeforeDelay       = 0f;
+        Duration          = 3f;
+        AfterDelay        = 0f;
+    }
+}
+
+public class DummySpeedBoostSkill : SkillBase
+{
+    public DummySpeedBoostSkill()
+    {
+        Id                = 10;
+        Type              = SkillType.General;
+        Priority          = 1;
+        IsRestricteMoving = false;
+        Cooldown          = 8f;
+        BeforeDelay       = 0f;
+        Duration          = 2f;
+        AfterDelay        = 0f;
+    }
+}
--- Assets/Scripts/Managers/Game/SkillManager.cs
+++ Assets/Scripts/Managers/Game/SkillManager.cs
@@ -7,6 +7,8 @@ public class SkillManager
 {
 	private Dictionary<Character, List<ISkill>> _skills = new();
 
+	private Dictionary<Character, List<ISkill>> _dummySkills = new(); // for test
+
 	private Dictionary<int, ISkill> _skillCache = new();
 
 	public List<ISkill> GeneratePool(int count)
@@ -43,5 +45,27 @@ public class SkillManager
 		}
 
 		return skill;
+	}
+
+
+	// for test
+	public void SetDummySkills()
+	{
+		Debug.Log("SetDummySkills");
+
+		_dummySkills.Add(new Character(), new List<ISkill>()
+		{
+			new DummyFireballSkill(),
+			new DummyHealSkill(),
+			new DummyStunSkill(),
+			new DummyShieldSkill(),
+			new DummyDashSkill(),
+			new DummyLightningSkill(),
+			new DummyIceSkill(),
+			new DummyStealthSkill(),
+			new DummyBarrierSkill(),
+			new DummySpeedBoostSkill()
+		});
+
 	}
 }
