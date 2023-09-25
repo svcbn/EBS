222ca477dd90087391f4ed4dec5940867aa6440d
LOONACIA <91052167+LOONACIA@users.noreply.github.com>
2023-09-25 21:43:16 +0900

    [Edit] 스킬 UI 수정

--- Assets/Scripts/UI/Item/UISkillSlot.cs
+++ Assets/Scripts/UI/Item/UISkillSlot.cs
@@ -10,7 +10,22 @@ public class UISkillSlot : UIBase
 		Icon
 	}
 
+	private static readonly Color s_SelectedColor = new(1, 0, 0, 0.5f);
+
+	private static readonly Color s_UnselectedColor = new(1, 1, 1, 0.5f);
+
+	private static readonly float s_SelectedScale = 1.2f;
+
 	private SkillPresentationData _item;
+
+	private Image _border;
+
+	protected override void Start()
+	{
+		base.Start();
+
+		_border = GetComponent<Image>();
+	}
 
 	public override void Init()
 	{
@@ -27,13 +42,26 @@ public class UISkillSlot : UIBase
 		_item = skill;
 	}
 
+	public void Select()
+	{
+		_border.color = s_UnselectedColor;
+	}
+
+	public void Unselect()
+	{
+		_border.color = s_SelectedColor;
+	}
+
 	private void SetSkillInfo()
 	{
 		var iconRoot = Get<GameObject>((int)Elements.Icon);
 		if (iconRoot.TryGetComponent<Image>(out var icon))
 		{
 			// TODO : 아이콘 이미지 변경
-			icon.sprite = _item.Info.Sprite;
+			if (_item.Info != null)
+			{
+				icon.sprite = _item.Info.Sprite;
+			}
 		}
 	}
 }
