75d30bff83f3600f85a9e3ca6715d9620720a989
Minjae <cyranokim@gmail.com>
2023-09-25 22:03:02 +0900

    Fix. Error of SlashData Scriptable Object

--- Assets/Scripts/Skills/Slash.cs
+++ Assets/Scripts/Skills/Slash.cs
@@ -45,29 +45,3 @@ public class Slash : SkillBase
 	}
 }
 
-[CreateAssetMenu(fileName = nameof(SlashData), menuName = "ScriptableObjects/Skills/" + nameof(SlashData))]
-public class SlashData : ScriptableObject
-{
-	[Header("OverlapBox")]
-	[Header("Check")]
-	[SerializeField]
-	private Vector2 _checkBoxCenter;
-
-	[SerializeField]
-	private Vector2 _checkBoxSize;
-
-	[Header("HitBox")]
-	[SerializeField]
-	private Vector2 _hitBoxCenter;
-
-	[SerializeField]
-	private Vector2 _hitBoxSize;
-
-	public Vector2 CheckBoxCenter => _checkBoxCenter;
-
-	public Vector2 CheckBoxSize => _checkBoxSize;
-
-	public Vector2 HitBoxCenter => _hitBoxCenter;
-
-	public Vector2 HitBoxSize => _hitBoxSize;
-}
