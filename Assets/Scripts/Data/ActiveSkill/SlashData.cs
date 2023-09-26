d21a25fda2183a6f83046447b334940cd9bf3914
ByeongHan Na <qudgks7946@gmail.com>
2023-09-26 11:21:42 +0900

    update scriptable objects

---
+++ Assets/Scripts/Data/ActiveSkill/SlashData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(SlashData), menuName = "ScriptableObjects/ActiveSkills/" + nameof(SlashData))]
+public class SlashData : ActiveSkillData
+{
+	
+}
