c644217782a193453f4be61e41f7c9dec7c9e984
Minjae <cyranokim@gmail.com>
2023-10-03 20:22:22 +0900

    Upgrade PassiveSkillBase.cs

--- Assets/Scripts/Skills/Abstractions/PassiveSkillBase.cs
+++ Assets/Scripts/Skills/Abstractions/PassiveSkillBase.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using UnityEngine;
@@ -10,7 +12,22 @@ public abstract class PassiveSkillBase : MonoBehaviour, IPassiveSkill
 	
 	public uint Id { get; protected set; }
 	
-	public Character Owner { get; set; }
+	private Character _owner;
+
+	public Character Owner
+	{
+		get => _owner;
+		set
+		{
+			if (_owner != null && _owner != value)
+			{
+				throw new InvalidOperationException($"Owner is already set. Owner: {_owner.name}");
+			}
+
+			_owner = value;
+			Debug.Log( $"Passive Owner : {_owner.name}" );
+		}
+	}
 	
 	public float Cooldown { get; protected set; }
 
@@ -44,8 +61,40 @@ public abstract class PassiveSkillBase : MonoBehaviour, IPassiveSkill
 	{
 	}
 
+	public virtual void Init(Character owner)
+	{
+		Owner = owner;
+
+	}
+
 	protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
 	{
 		PropertyChanged?.Invoke(this, new(propertyName));
+	}
+
+	protected void PlayEffect(string effName, float duration, Transform parent, Vector2 offset, float sign = 1)
+	{
+		StartCoroutine(PlayEffectCo(effName, duration, parent, offset, sign));
+	}
+
+	IEnumerator PlayEffectCo(string effName, float duration, Transform parent, Vector2 offset, float sign)
+	{
+		GameObject effect = Managers.Resource.Instantiate("Skills/"+effName, parent ); // paraent를 character.gameObject로
+		
+		if(effect){
+			effect.transform.localPosition = Vector3.zero;
+		}else{
+			Debug.LogError($"effect is null. effName :{effName}");
+		}
+
+		effect.transform.localPosition += (Vector3)offset;
+
+        // todo 크기 변경 필요 : 이펙트 크게 나오는 이유임.
+		effect.transform.localScale = new Vector3(sign * Owner.transform.localScale.x, Owner.transform.localScale.y, Owner.transform.localScale.z); 
+        
+
+		yield return new WaitForSeconds(duration); // 이펙트 재생 시간
+		Managers.Resource.Release(effect);
+		
 	}
 }
