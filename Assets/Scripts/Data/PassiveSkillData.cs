fc3ad62a1305ea0a1e816eeb011a0a5cd80755fa
Minjae <cyranokim@gmail.com>
2023-10-03 20:18:38 +0900

    Add SkillType in PassiveSkillData.cs

--- Assets/Scripts/Data/PassiveSkillData.cs
+++ Assets/Scripts/Data/PassiveSkillData.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PassiveSkillData : ScriptableSkillData
 {
+	[SerializeField] private SkillType _type;
+
 	[SerializeField] private float _amount;
 
 	public float Amount => _amount;
