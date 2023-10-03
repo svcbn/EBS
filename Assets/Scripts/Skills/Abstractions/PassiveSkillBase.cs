5bad90f9759c88844cae382b35872bbbf8c64475
ByeongHan Na <qudgks7946@gmail.com>
2023-10-03 22:26:34 +0900

    Update PassiveSkillBase.cs

--- Assets/Scripts/Skills/Abstractions/PassiveSkillBase.cs
+++ Assets/Scripts/Skills/Abstractions/PassiveSkillBase.cs
@@ -28,6 +28,24 @@ public abstract class PassiveSkillBase : MonoBehaviour, IPassiveSkill
 		}
 	}
 	
+
+
+
+	public Character Owner
+	{
+		get => _owner;
+		set
+		{
+			if (_owner != null && _owner != value)
+			{
+				throw new InvalidOperationException($"Owner is already set. Owner: {_owner.name}");
+			}
+
+			_owner = value;
+		}
+	}
+
+
 	public float Cooldown { get; protected set; }
 
 	public bool IsEnabled
@@ -42,7 +60,7 @@ public abstract class PassiveSkillBase : MonoBehaviour, IPassiveSkill
 	}
 
 	public float CurrentCooldown { get; protected set; }
-	
+
 	public bool HasPresentNumber { get; protected set; }
 
 	public int PresentNumber
@@ -55,9 +73,9 @@ public abstract class PassiveSkillBase : MonoBehaviour, IPassiveSkill
 			RaisePropertyChanged();
 		}
 	}
-	
+
 	public event PropertyChangedEventHandler PropertyChanged;
-	
+
 	public virtual void Init()
 	{
 	}
@@ -65,5 +83,33 @@ public abstract class PassiveSkillBase : MonoBehaviour, IPassiveSkill
 	protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
 	{
 		PropertyChanged?.Invoke(this, new(propertyName));
+	}
+
+	protected void PlayEffect(string effName, float duration, float sign = 1, Vector2 offset = default)
+	{
+		StartCoroutine(PlayEffectCo(effName, duration, sign, offset));
+	}
+
+	IEnumerator PlayEffectCo(string effName, float duration, float sign, Vector2 offset)
+	{
+		
+		Transform parent = Owner.transform;
+		GameObject effect = Managers.Resource.Instantiate("Skills/" + effName, parent); // paraent를 character.gameObject로
+
+		if (effect)
+		{
+			effect.transform.localPosition = Vector3.zero;
+		}
+		else
+		{
+			Debug.LogError($"effect is null. effName :{effName}");
+		}
+
+		effect.transform.localPosition += (Vector3)offset;
+		effect.transform.localScale = new Vector3(sign * Owner.transform.localScale.x, Owner.transform.localScale.y, Owner.transform.localScale.z);
+
+		yield return new WaitForSeconds(duration); // 이펙트 재생 시간
+		Managers.Resource.Release(effect);
+
 	}
 }
